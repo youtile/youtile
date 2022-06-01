d2e15ec9f67e9c3fde1691d2082616c6df51572d
Max Coppen <44031065+mxcop@users.noreply.github.com>
2022-06-01 23:12:32 +0200

    Stream Resolutions

--- src-tauri/src/stream.rs
+++ src-tauri/src/stream.rs
@@ -1,5 +1,21 @@
+use ytextract::{Video, Stream};
+
+#[derive(serde::Serialize)]
+pub struct StreamResolution {
+  width: u16,
+  height: u16,
+  mine: String,
+}
+
+#[derive(serde::Serialize)]
+pub struct StreamCouple {
+  video_stream: String,
+  audio_stream: String,
+  resolution: StreamResolution,
+}
+
 #[tauri::command]
-pub async fn stream_video(code: String) -> String {
+pub async fn stream_video(code: String) -> Vec<StreamCouple> {
   // Get a Client for making request
   let client = ytextract::Client::new();
 
@@ -11,16 +27,21 @@ pub async fn stream_video(code: String) -> String {
   let mut bestvid = String::new();
   let mut bestaud = String::new();
   for stream in streams {
-    if stream.is_video() {
-      println!("video stream: {}b {}", stream.bitrate(), stream.mime_type());
-      if bestvid == String::new() {
-        bestvid = stream.url().to_string();
-      }
-    }
-    if stream.is_audio() && bestaud == String::new() {
-      bestaud = stream.url().to_string();
+    match stream {
+        Stream::Video(stream) => {
+          println!("{}/{} : {}", stream.width(), stream.height(), stream.mime_type());
+          if bestvid == String::new() {
+            bestvid = stream.url().to_string();
+          }
+        },
+        Stream::Audio(stream) => {
+          if bestaud == String::new() {
+            bestaud = stream.url().to_string();
+          }
+        }
     }
   }
 
-  format!("{} {}", bestvid, bestaud)
+  Vec::new() // TODO: Make the stream couples and return them.
+  //format!("{} {}", bestvid, bestaud)
 }
